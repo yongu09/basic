a41bba912c791fcb00d3c42013f9704ccc5feca5
yongu09 <klfm2415@naver.com>
2025-03-07 16:51:41 +0900

    2025.03.07. 16:50

--- src/main/java/com/korit/basic/controller/SecurityController.java
+++ src/main/java/com/korit/basic/controller/SecurityController.java
@@ -1,6 +1,8 @@
 package com.korit.basic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,14 @@ public class SecurityController {
   ) {
     String jwt = jwtProvider.create(name);
     return jwt;
+  }
+
+  @PostMapping("/jwt")
+  public String validateJwt(
+    @RequestBody String jwt
+  ) {
+    String subject = jwtProvider.validate(jwt);
+    return subject;
   }
 
 }
--- src/main/java/com/korit/basic/provider/JwtProvider.java
+++ src/main/java/com/korit/basic/provider/JwtProvider.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -44,9 +45,8 @@ public class JwtProvider {
 
   // JWT 생성 메서드
   public String create(String name) {
-
+    
     // 비밀키 객체 생성
-
     Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
     // JWT 만료 시간
@@ -67,5 +67,31 @@ public class JwtProvider {
       .compact();
 
       return jwt;
+
   }
+
+  // JWT 검증 메서드
+    public String validate(String jwt) {
+
+      // JWT 검증 결과로 반환 받는 페이로드(=Claims)가 저장될 변수
+      Claims claims = null;
+      // 비밀키 객체 생성
+      Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+      try {
+
+        claims = Jwts.parserBuilder()
+          .setSigningKey(key)
+          .build()
+          .parseClaimsJws(jwt)
+          .getBody();
+
+      } catch(Exception exception) {
+        exception.printStackTrace();
+        return null;
+      }
+
+      return claims.getSubject();
+      
+    }
 }
